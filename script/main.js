e5ce56621c9f4afcdde419276ee9a9d10fc42293
Fernando jose Mejia <>
2020-10-18 19:23:27 -0400

    Proyecto listo

--- script/main.js
+++ script/main.js
@@ -1,4 +1,5 @@
 function carrusel() {
+    fillSkill()
     imagenes= ["img/pexels-photo-115655.jpeg","img/pexels-photo-461064.jpeg","img/pexels-photo-930530.jpeg"];
     i=0;
     window.setInterval(function(){
@@ -6,4 +7,71 @@ function carrusel() {
         i++;
         if(i>2) i=0;
     },8000);
+}
+/*
+function loadJson(callback){
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'script/skills.json', true);
+    xobj.onreadystatechange = function() {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            callback(xobj.responseText);
+            
+        }
+    }
+    xobj.send(null);
+
+}
+
+
+
+function fillSkill(){
+    var padre = document.getElementById("contenido");
+    var mydata = JSON.parse(data);
+    console.log(mydata);
+    var node = document.createElement("div");
+    node.classList.add("grafico");
+    var strg = document.createElement("strong");
+    var textN = document.createTextNode("fernando75%");
+    strg.appendChild(textN);
+    strg.classList.add("barra");
+    strg.style.width="75%";
+    node.appendChild(strg);
+    padre.appendChild(node);
+    
+}*/
+document.getElementById("formulario").onsubmit = function(){
+    var x = document.getElementById("name").value;
+    var valido = true;
+    var mensaje = "";
+    if (x == "") {
+      mensaje+="\nEl campo nombre está vacio"
+      valido = false;
+    }else{
+        if(!/^[A-Za-z]+$/.test(x)){
+            mensaje += "\nEl campo nombre es inválido";
+            valido = false;
+        }
+    }
+    x = document.getElementById("msg").value;
+    if (x == "") {
+        mensaje+="\nEl campo mensaje está vacio"
+        valido = false;
+    }
+    x = document.getElementById("mail").value;
+    if (x == "") {
+        mensaje+="\nEl campo email está vacio"
+        valido = false;
+    }else{
+        if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(x)){
+            mensaje += "\nEl campo email es inválido";
+            valido = false;
+        }
+    }
+    if(!valido){
+        alert(mensaje);
+    }else{ 
+        alert("su mensaje ha sido enviado")
+    }
+    return valido;
 }
